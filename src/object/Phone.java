03fce014279f710942b02c1b87f37989e3c1c9a2
Gahyeon19 <wjsrkgus0119@naver.com>
2024-08-14 17:27:16 +0900

    캡슐화_Phone Object

--- src/object/Phone.java
+++ src/object/Phone.java
@@ -2,9 +2,26 @@ package object;
 
 class Phone {
   //필드(변수)
-  String model;
-  int price;
+  private String model;
+  private int price;
+
   //매서드
+  public String getModel(){
+    return model;
+  }
+
+  public void setModel(String modelname){
+    this.model = modelname;
+  }
+
+  public int getPrice(){
+    return price;
+  }
+
+  public void setPrice(int price1){
+    this.price = price1;
+  }
+
   void print(){
     System.out.println(price + "원의 모델 " + model + " 휴대폰입니다.");
   }
--- src/object/PhoneDemo.java
+++ src/object/PhoneDemo.java
@@ -5,12 +5,12 @@ public class PhoneDemo {
     Phone p1 = new Phone();
     Phone p2 = new Phone();
 
-    p1.model = "갤럭시 Z 폴드 6";
-    p1.price = 2_586_000;
+    p1.setModel("갤럭시 Z 폴드 6");
+    p1.setPrice(2_586_000);
     p1.print();
 
-    p2.model = "아이폰 15 Pro";
-    p2.price = 2_320_000;
+    p2.setModel("아이폰 15 Pro");
+    p2.setPrice(2_320_000);
     p2.print();
 
   }
