9a52cab5fc3895fd93a84e3d42b83bf9db5e39e7
Gahyeon19 <wjsrkgus0119@naver.com>
2024-08-16 15:07:01 +0900

    생성자 오버로딩

---
+++ src/object/ColoredCircleDemo.java
@@ -0,0 +1,63 @@
+package object;
+
+public class ColoredCircleDemo {
+  public static void main(String[] args) {
+    ColoredCircle cc1 = new ColoredCircle();
+    ColoredCircle cc2 = new ColoredCircle("Yellow");
+    ColoredCircle cc3 = new ColoredCircle(10);
+    ColoredCircle cc4 = new ColoredCircle(9, "Blue");
+
+    System.out.printf("1번 원의 반지름 : %d, 색깔 : %s\n", cc1.r, cc1.color);
+    System.out.printf("2번 원의 반지름 : %d, 색깔 : %s\n", cc2.r, cc2.color);
+    System.out.printf("3번 원의 반지름 : %d, 색깔 : %s\n", cc3.r, cc3.color);
+    System.out.printf("4번 원의 반지름 : %d, 색깔 : %s\n", cc4.r, cc4.color);
+  }
+}
+
+class ColoredCircle{
+  //멤버 - 필드
+  int r;
+  String color;
+
+  //생성자 - 오버로딩
+  public ColoredCircle(){
+//    color = "white";
+    this(1,"White");
+  }
+  public ColoredCircle(String color){
+//    if (color == null || color.equals("") || color.equals(" ")){  //isEmpty() , isBlank()
+////      this.color = "white";
+//    }
+//    else {
+//      this.color = color;
+//    }
+    this(1, color);
+  }
+  public ColoredCircle(int radius){
+//    if (radius > 0){
+//      this.r = radius;
+//    }
+    this(radius, "White");
+//    this.color = "white";
+  }
+  public ColoredCircle(int radius, String color){
+    if (radius > 0){
+      this.r = radius;
+    }
+    else {
+      this.r = 1;
+    }
+
+    if (color == null || color.equals("") || color.equals(" ")){  //isEmpty() , isBlank()
+      this.color = "White";
+    }
+    else {
+      this.color = color;
+    }
+
+  }
+
+  //멤버 - 매서드
+
+
+}
